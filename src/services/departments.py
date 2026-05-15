9b8f6e6e35e566a6b8f7e18aa61e5cb44b7bcde0
Arthur <arthur155188@gmail.com>
2026-05-15 23:18:53 +0500

    Добавлен метод get_department_with_check - проверяет существование подразделение

--- src/services/departments.py
+++ src/services/departments.py
@@ -1,9 +1,22 @@
-from src.exceptions.exceptions import ObjectAlreadyExistsException
+from src.exceptions.exceptions import (
+    ObjectAlreadyExistsException,
+    ObjectNotFoundException,
+    DepartmentNotFoundException,
+)
 from src.schemas.departments import DepartmentAdd
 from src.services.base import BaseService
 
 
 class DepartmentService(BaseService):
+    async def get_department_with_check(self, department_id: int):
+        """
+        Проверяет существование подразделение
+        """
+        try:
+            await self.db.departments.get_one(id=department_id)
+        except ObjectNotFoundException:
+            raise DepartmentNotFoundException
+
     async def create_department(self, data: DepartmentAdd):
         """
         Создает подразделение
@@ -13,6 +26,7 @@ class DepartmentService(BaseService):
         )
         if existing_department:
             raise ObjectAlreadyExistsException
+        await self.get_department_with_check(data.parent_id)
         department = await self.db.departments.add(data)
         await self.db.commit()
         return department
