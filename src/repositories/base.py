eb03875697b291c50a959877fbbd7bf38651b3c1
Arthur <arthur155188@gmail.com>
2026-05-15 22:03:26 +0500

    Добавлен базовый репозиторий с методами:
    - get_filtered - Возвращает отфильтрованные данные;
    - add - Добавляет данные в БД;
    - edit - Изменяет данные в БД;
    - delete - Удаляет данные из БД.

---
+++ src/repositories/base.py
@@ -0,0 +1,65 @@
+import logging
+
+from asyncpg import UniqueViolationError
+from pydantic import BaseModel
+from sqlalchemy import insert, update, delete, select
+
+from src.exceptions.exceptions import ObjectAlreadyExistsException
+from src.repositories.mappers.base import DataMapper
+
+
+class BaseRepository:
+    model = None
+    mapper: DataMapper
+
+    def __init__(self, session):
+        self.session = session
+
+    async def get_filtered(self, *filter, **filter_by):
+        """
+        Возвращает отфильтрованные данные
+        """
+        query = select(self.model).filter(*filter).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        return [self.mapper.map_to_domain_entity(obj) for obj in result.scalars().all()]
+
+    async def add(self, data: BaseModel):
+        """
+        Добавляет данные в бд
+        """
+        try:
+            add_stmt = (
+                insert(self.model).values(**data.model_dump()).returning(self.model)
+            )
+            result = await self.session.execute(add_stmt)
+            obj = result.scalars().one()
+            return self.mapper.map_to_domain_entity(obj)
+        except InterruptedError as e:
+            logging.error(
+                f"Не удалось добавить данные в БД, тип ошибки: {type(e.orig.__cause__)=}"
+            )
+            if isinstance(e.orig.__cause__, UniqueViolationError):
+                raise ObjectAlreadyExistsException from e
+            else:
+                logging.error(
+                    f"Незнакомая ошибка, тип ошибки: {type(e.orig.__cause__)=}"
+                )
+                raise e
+
+    async def edit(self, data: BaseModel, exclude_unset: bool = False, **filter_by):
+        """
+        Изменяет данные в БД, если exclude_unset = True, то изменяет частично
+        """
+        edit_stmt = (
+            update(self.model)
+            .filter_by(**filter_by)
+            .values(**data.model_dump(exclude_unset=exclude_unset))
+        )
+        await self.session.execute(edit_stmt)
+
+    async def delete(self, **filter_by):
+        """
+        Удаляет данные из БД
+        """
+        delete_stmt = delete(self.model).filter_by(**filter_by)
+        await self.session.execute(delete_stmt)
