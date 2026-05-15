790469e0871df063797128e12e5c16e05d9556de
Arthur <arthur155188@gmail.com>
2026-05-15 21:00:06 +0500

    Добавлена dependency для работы с БД через DBManager

---
+++ src/api/dependencies/dependencies.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+from fastapi import Depends
+
+from src.database import async_session_maker
+from src.utils.db_manager import DBManager
+
+
+async def get_db():
+    """
+    Создает асинхронную сессию базы данных через DBManager
+    и автоматически закрывает её после завершения запроса
+
+    yield:
+        DBManager: Экземпляр менеджера базы данных
+    """
+    async with DBManager(session_factory=async_session_maker) as db:
+        yield db
+
+
+DBDep = Annotated[DBManager, Depends(get_db)]
