be7f5ec69906a50586a6ca9fd5e8b9e4e7ddff89
Arthur <arthur155188@gmail.com>
2026-05-16 23:20:02 +0500

    Добавлены метод get_employees_from_department который позволяет получить работников подразделения

--- src/repositories/employees.py
+++ src/repositories/employees.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from src.models import EmployeesOrm
 from src.repositories.base import BaseRepository
 from src.repositories.mappers.employees import EmployeeDataMapper
@@ -6,3 +8,15 @@ from src.repositories.mappers.employees import EmployeeDataMapper
 class EmployeesRepository(BaseRepository):
     model = EmployeesOrm
     mapper = EmployeeDataMapper
+
+    async def get_employees_from_department(self, department_id: int):
+        """
+        Получить работников подразделения
+        """
+        query = (
+            select(self.model)
+            .filter_by(department_id=department_id)
+            .order_by(self.model.created_at)
+        )
+        result = await self.session.execute(query)
+        return [self.mapper.map_to_domain_entity(obj) for obj in result.scalars().all()]
