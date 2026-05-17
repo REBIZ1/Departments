33d3224370839961229df2593db573397f050ee6
Arthur <arthur155188@gmail.com>
2026-05-17 21:05:09 +0500

    Добавлены методы:
    - validate_no_cycle - проверяет на цкл в дереве;
    - update_department - Изменяет подразделение.

--- src/services/departments.py
+++ src/services/departments.py
@@ -2,8 +2,10 @@ from src.exceptions.exceptions import (
     ObjectAlreadyExistsException,
     ObjectNotFoundException,
     DepartmentNotFoundException,
+    DepartmentCannotBeSelfChildException,
+    DepartmentHierarchyLoopException,
 )
-from src.schemas.departments import DepartmentAdd, DepartmentTree
+from src.schemas.departments import DepartmentAdd, DepartmentTree, DepartmentPatch
 from src.services.base import BaseService
 
 
@@ -73,3 +75,38 @@ class DepartmentService(BaseService):
         return await self._build_tree(
             department=department, depth=depth, include_employees=include_employees
         )
+
+    async def validate_no_cycle(self, department_id: int, parent_id: int):
+        """
+        Проверка на цикл в дереве
+        """
+        current_parent_id = parent_id
+        while current_parent_id is not None:
+            if current_parent_id == department_id:
+                raise DepartmentHierarchyLoopException
+            department = await self.db.departments.get_one(id=current_parent_id)
+            current_parent_id = department.parent_id
+
+    async def update_department(self, department_id: int, data: DepartmentPatch):
+        """
+        Изменяет подразделение
+        """
+        await self.get_department_with_check(department_id)
+        update_data = data.model_dump(exclude_unset=True)
+        if "parent_id" in update_data:
+            parent_id = update_data["parent_id"]
+            if parent_id == department_id:
+                raise DepartmentCannotBeSelfChildException
+            if parent_id is not None:
+                await self.get_department_with_check(parent_id)
+                await self.validate_no_cycle(
+                    department_id=department_id,
+                    parent_id=parent_id
+                )
+        updated_department = await self.db.departments.edit(
+            data=data,
+            exclude_unset=True,
+            id=department_id
+        )
+        await self.db.commit()
+        return updated_department
